9dc9d241a0d043332c95c20ef5de3ba1c84bec2f
andreluizang <andreluizgbi@gmail.com>
2021-08-27 11:25:29 -0300

    Adicionei mais uma linha ao código

--- mynewproject/src/mynewproject/mynewproject.java
+++ mynewproject/src/mynewproject/mynewproject.java
@@ -5,6 +5,8 @@ public class mynewproject {
 	public static void main (String arts[]) {
 		// Criação do projeto
 		System.out.println("Hello World!");
+		// Fazendo o push
+		System.out.println("Vou fazer o push");
 	}
 
 }
